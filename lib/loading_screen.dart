5d79a3ff4d6be0462f0cc56c84c26358baa14876
Amal A M <vectorhero007@gmail.com>
2025-06-16 19:44:09 +0530

    testing openweather api perfectly running status 200

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 import 'location.dart';
 
@@ -13,15 +14,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   void getLocation() async {
-    
+
     Location location = await Location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
-
   }
-  
+
+  void getData() async {
+    final url = Uri.parse(
+      'https://api.openweathermap.org/data/2.5/weather?lat=44,34&lon=10.99&appid=20a7a2abb54b0ee9ad26eb39e27b34ea',
+    );
+
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      print('✅ Data: ${response.body}');
+    } else {
+      print('❌ Failed with status: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,6 +66,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
             GestureDetector(
               onTap: () {
                 getLocation();
+                getData();
               },
               child: Container(
                 margin: EdgeInsets.symmetric(horizontal: 20.0),
